9630b1e4605bcd8432888717d525cb99306e8c98
s809 <43530948+s809@users.noreply.github.com>
2024-11-19 13:25:36 +0500

    Add files

---
+++ app/Helpers/QueryHelper.php
@@ -0,0 +1,23 @@
+<?
+
+class QueryHelper
+{
+
+    public function __construct($Model, $config)
+    {
+        // ... Реализуйте метод
+    }
+
+    // Создание запроса QueryBuilder'a
+    public function applyOptions($userOptions)
+    {
+        // ... Реализуйте метод
+    }
+
+    // Получение скомпилированного запроса QueryBuilder'a
+    public function getQuery()
+    {
+        // ... Реализуйте метод
+    }
+
+}
---
+++ app/Services/Test.php
@@ -0,0 +1,53 @@
+<?
+
+class Test
+{
+
+    public function runTest()
+    {
+
+        $config = [
+            // ... Ваша реализация конфига для преобразования массива фильтров в запрос QueryBuilder'a
+        ];
+
+        /*
+        !!! Обратите внимание,
+        что наименования ключей, формат данных в объекте filters не всегда соответствует
+        наименованию и значению фильтруемой колонки в БД.
+        Это сделано намеренно. Просим учесть это при реализации методов класса QueryHelper и структуры $config'а.
+        */
+        $testData = [
+            'items' => [
+                [
+                    'filters' => [
+                        'ids' => [1, 2, 3], // posts.id - id публикаций (int | string | array)
+                        'tags' => ['news', 'articles'], // tags.slug - привязанные теги публикаций (string | array)
+                        'active' => true, // posts.is_active - статус публикаций (boolean)
+                        'author' => 1, // posts.owner_id - владелец публикаций (int | string | array)
+                        'create_date_range' => ['26-03-2024', '27-03-2024'], // posts.created_at - дата создания публикации (array)
+                    ],
+                    'order' => ['id', 'desc'], // сортировка записей
+                ],
+            ],
+        ];
+
+        $testResults = [];
+        foreach ($testData['items'] as $userOptions) {
+
+            $Model = new Posts();
+            $QueryHelper = new QueryHelper($Model, $config);
+            $QueryHelper->applyOptions($userOptions); // Создание запроса QueryBuilder'a
+
+            $q = $QueryHelper->getQuery(); // Получение скомпилированного запроса QueryBuilder'a
+            $items = $q->get(); // Получение публикаций
+            $testResults[] = $items->toArray();
+
+        }
+
+        dump($testResults);
+
+        // !!! Запись результатов в zip-архив
+
+    }
+
+}
